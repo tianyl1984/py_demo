428910e852e70ea42d0523bb4ebd532045ded5ee
tianyl1984 <tianyl1984@qq.com>
2015-04-25 15:47:59 +0800

    添加http和json库例子

---
+++ jsonDemo.py
@@ -0,0 +1,20 @@
+﻿#coding:utf-8
+import urllib
+import urllib2
+import json
+
+url = "https://api.douban.com/v2/movie/search?q=%E5%8D%81%E9%9D%A2"
+
+req = urllib2.Request(url)
+res_data = urllib2.urlopen(req)
+res = res_data.read()
+
+res_json = json.loads(res)
+
+print res_json["total"]
+
+for (i,subject) in enumerate(res_json["subjects"]):
+	print subject["id"] + ":" + subject["title"]
+
+
+
